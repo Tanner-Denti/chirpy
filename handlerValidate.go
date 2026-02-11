f352168c4c3ac767ca3eb4d294f9e00910b3a1ef
Tanner-Denti <tannerwdenti@gmail.com>
2026-02-10 19:53:07 -0700

    Create basic endpoints to understand go servers better

---
+++ handlerValidate.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"net/http"
+	"encoding/json"
+	"io"
+	"strings"
+)
+
+func handlerValidateChirpPost(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	type requestBody struct {
+		Body string `json:"body"`
+	}
+	type responseBody struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
+	reqBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		respondWithError(w, 500, "Could not read request", err) // How should I handle this error if it returns one?
+		return
+	}
+	
+	var req requestBody
+	err = json.Unmarshal(reqBody, &req)
+	if err != nil {
+		respondWithError(w, 500, "Could not unmarshal parameters", err) // How should I handle this error if it returns one?
+		return
+	}
+
+	if len(req.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long", nil)
+		return
+	}
+
+	cleanedBody := replaceBadWords(req.Body)
+
+	respondWithJSON(w, 200, responseBody{CleanedBody: cleanedBody})
+}
+
+func replaceBadWords(s string) string {
+	words := strings.Fields(s)
+	for i, word := range words {
+		lowercase := strings.ToLower(word)
+		if lowercase == "kerfuffle" || lowercase == "sharbert" || lowercase == "fornax" {
+			words[i] = "****"
+		}
+	}
+
+	return strings.Join(words, " ")
+}
--- main.go
+++ main.go
@@ -3,17 +3,41 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
-	serverMux := http.NewServeMux()
-	serverMux.Handle("/", http.FileServer(http.Dir(".")))
-	server := &http.Server{
-		Addr: ":8080",
-		Handler: serverMux,
+	const filepathRoot = "."
+	const port = "8080"
+
+	apiCfg := apiConfig { fileserverHits: atomic.Int32{} }
+
+	mux := http.NewServeMux()
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", fsHandler)
+
+	mux.HandleFunc("GET /api/healthz", handlerHealthzGet)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirpPost)
+
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetricsGet)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerResetGet)
+
+	srv := &http.Server{
+		Addr: ":" + port,
+		Handler: mux,
 	}
-
-
-	log.Fatal(server.ListenAndServe())
+	
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
+
+
+
+
+
+
+
